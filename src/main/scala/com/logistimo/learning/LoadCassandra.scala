f08451bdf27fbed7e191616557a79636bd034596
mohansrinivas <mohansrinivas@logistimo.com>
2016-06-02 15:00:13 +0530

    mysql read and load to cassandra

--- src/main/scala/com/logistimo/learning/LoadCassandra.scala
+++ src/main/scala/com/logistimo/learning/LoadCassandra.scala
@@ -22,7 +22,7 @@ object LoadCassandra {
     val conf = new SparkConf().setAppName("CassandraLoader").setMaster(master).set("spark.cassandra.connection.host", cassandra_host).set("spark.eventLog.enabled", "true")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val data = sc.textFile("hdfs://cassandra1:9000/cassandra/dataextracted/part-0000*").map(_.split(","))
+    val data = sc.textFile("hdfs://cassandra1:9000/cassandra/dataextracted/part-0000*")
     data.saveToCassandra("testdb","dayslice_user")
     //val testData = sc.wholeTextFiles(inputDir).map(_.split(","))
     //val data = testData.map(p => Trend(p(0).trim.toLong, Timestamp.valueOf((p(1))), p(2).trim.toLong, p(3).trim.toLong, p(4).trim.toDouble, p(5).trim.toDouble, p(6)))
