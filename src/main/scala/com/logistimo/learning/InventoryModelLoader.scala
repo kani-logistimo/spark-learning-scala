4fa236da723d9fceae0ace6f76b393c5ff04fa6b
mohansrinivas <mohansrinivas@logistimo.com>
2016-06-03 11:12:22 +0530

    mysql read and load to cassandra

--- src/main/scala/com/logistimo/learning/InventoryModelLoader.scala
+++ src/main/scala/com/logistimo/learning/InventoryModelLoader.scala
@@ -8,41 +8,29 @@ import com.datastax.spark.connector._
  */
 object InventoryModelLoader {
   def main (args : Array[String]): Unit = {
-    if(args.length != 5){
+    if(args.length != 3){
       System.out.println("Invalid Input Parameters")
-      System.out.println("Usage InventoryModelLoader masterip  cassandrahost cassandr_-keyspace cassandra_table inputfile ")
+      System.out.println("Usage InventoryModelLoader masterURL   inputFile outputDir ")
       System.exit(1)
     }
     val master = args(0)
-    val cassandraHost = args(1)
-    val inputFile = args(4)
-    val casssandraKeySpace = args(2)
-    val cassandraTable = args(3);
+    val inputFile = args(1)
+    val outputDir = args(2)
 
-    val conf = new SparkConf().setAppName("CassandraLoader").setMaster(master).set("spark.cassandra.connection.host", cassandraHost).set("spark.eventLog.enabled", "true")
+
+    val conf = new SparkConf().setAppName("HDFS Loader").setMaster(master).set("spark.eventLog.enabled", "true")
     val sc = new SparkContext(conf)
-
     val lines = sc.textFile(inputFile)
-
-
     val finalOutput = lines.map(
       line => map(line)).reduceByKey{
       case(x, y) => reduce(x, y)
     }
-    //finalOutput.values.saveToCassandra(casssandraKeySpace,cassandraTable)
-    finalOutput.values.saveAsTextFile("hdfs://cassandra1:9000/cassandra/dataextracted")
+    finalOutput.values.saveAsTextFile(outputDir)
   }
 
   def map(line: String):(String, InventoryModel) ={
     val lineArray = line.split(",")
-    /*ty match {
-      case "M" => {*/
-        (lineArray(0) + "-" + lineArray(1) + "-" + lineArray(4), new InventoryModel(lineArray(0), lineArray(1), lineArray(4), lineArray(6), 1))
-     /* }
-      case "K" => { (lineArray(0) + "-" + lineArray(2) + "-" + lineArray(4), new InventoryModel(lineArray(0), lineArray(2), lineArray(4), lineArray(6), 0))}
-      case "U" => { (lineArray(0) + "-" + lineArray(1) + "-" + lineArray(4), new InventoryModel(lineArray(0), lineArray(1), lineArray(4), lineArray(6), 0))}*/
-
-   // }
+    (lineArray(0) + "-" + lineArray(1) + "-" + lineArray(4), new InventoryModel(lineArray(0), lineArray(1), lineArray(4), lineArray(6), 1))
   }
 
   def reduce(x: InventoryModel,y:InventoryModel):InventoryModel= {
--- src/main/scala/com/logistimo/learning/LoadCassandra.scala
+++ src/main/scala/com/logistimo/learning/LoadCassandra.scala
@@ -15,16 +15,19 @@ object LoadCassandra {
   case class dayslice_user(did:String,id:String,date:String,sq:BigDecimal,tc:Long)
   def main(args: Array[String]): Unit = {
 
-    /*//val conf = new SparkConf().setAppName("CassandraLoader").setMaster(master).set("spark.cassandra.connection.host", cassandra_host).set("spark.eventLog.enabled", "true")
+    val master = args(0)
+    val cassandraHost = args(1)
+    val cassandraKeyspace = args(2)
+    val cassandraTable = args(3)
+    val inputFile = args(4)
+
+    val conf = new SparkConf().setAppName("CassandraLoader").setMaster(master).set("spark.cassandra.connection.host", cassandraHost).set("spark.eventLog.enabled", "true")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val data = sc.textFile("hdfs://cassandra1:9000/cassandra/dataextracted/part-0000*").map(_.split(","))
-    //val testData = sc.wholeTextFiles(inputDir).map(_.split(","))
-    val dataa = data.map(p=>dayslice_user(p(0),p(1),p(2),p(3).toLong,p(4).toLong))
-    dataa.saveToCassandra("testdb","dayslice_user")*/
+    val data = sc.textFile(inputFile).map(_.split(","))
+    val userData = data.map(p=>dayslice_user(p(0),p(1),p(2),p(3).toLong,p(4).toLong))
+    userData.saveToCassandra(cassandraKeyspace,cassandraTable)
 
-    //val data = testData.map(p => Trend(p(0).trim.toLong, Timestamp.valueOf((p(1))), p(2).trim.toLong, p(3).trim.toLong, p(4).trim.toDouble, p(5).trim.toDouble, p(6)))
-    //data.saveToCassandra("spark_aggr", "material_trend", columns = SomeColumns("domain_id", "t", "mid", "kid", "cs", "q", "ty"))
   }
 }
 
